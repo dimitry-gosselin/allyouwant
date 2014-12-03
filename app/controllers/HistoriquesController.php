6eefceb5a9addf1b3f062aacc7973b6bc5ef6b9f
lemirage <william91120@hotmail.com>
2014-12-03 14:55:29 +0100

    Create HistoriquesController.php

---
+++ app/controllers/HistoriquesController.php
@@ -0,0 +1,25 @@
+<?php
+class HistoriquesController extends BaseController  {
+
+  public function index()
+  {
+
+
+  }
+  public function view($id)
+  {
+
+
+  }
+  public function edit($id)
+  {
+
+
+  }
+  public function update($id)
+  {
+
+
+  }
+
+}
