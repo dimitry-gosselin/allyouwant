150bb447c35f630b18b07e5e292ae70c15bdfb52
lemirage <william91120@hotmail.com>
2014-12-03 14:43:31 +0100

    Create UsersController.php
    Controller user

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+class UsersController extends BaseController  {
+
+  public function index()
+  {
+    //$posts = User::paginate(5);
+    //return View::make('posts.index',compact('posts'));
+  }
+  
+  public function view($id)
+  {
+  }
+  
+  public function edit($id)
+  {
+  }
+  
+  public function update($id)
+  {
+  }
+  
+}
