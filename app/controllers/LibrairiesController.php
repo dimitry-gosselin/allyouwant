923af73b346922d41182713186eae7b85d0f1ad8
lemirage <william91120@hotmail.com>
2014-12-03 14:50:46 +0100

    Create LibrairiesController.php
    librairie

---
+++ app/controllers/LibrairiesController.php
@@ -0,0 +1,22 @@
+<?php
+class LibrairiesController extends BaseController  {
+
+  public function index()
+  {
+    //$posts = Librairies::paginate(5);
+    //return View::make('posts.index',compact('posts'));
+  }
+  
+  public function view($id)
+  {
+  }
+  
+  public function edit($id)
+  {
+  }
+  
+  public function update($id)
+  {
+  }
+  
+}
